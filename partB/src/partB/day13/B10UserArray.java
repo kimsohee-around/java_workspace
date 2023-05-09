c95dfdf6525972c9be328d993642176b25796793
kimsohee-around <koreait.sec2020@gmail.com>
2023-05-09 15:42:31 +0900

    #13 객체 배열 연습

--- partB/src/partB/day13/B10UserArray.java
+++ partB/src/partB/day13/B10UserArray.java
@@ -1,4 +1,7 @@
 package partB.day13;
+
+import java.util.Scanner;
+
 //객체의 배열 연습.   String 배열을 참고하세요.
 public class B10UserArray {
 	public static void main(String[] args) {
@@ -48,6 +51,63 @@ public class B10UserArray {
 				System.out.println(twice[i].user());
 		}
 		
+		
+		//4. 배열에 참조하는 객체는 User 타입입니다. - User 객체의 name필드만 출력하기
+		System.out.println("==twice 배열에 저장된 User 객체의 name 필드 출력==");
+		for(int i=0;i<twice.length;i++) {
+			if(twice[i] != null)
+				System.out.println(twice[i].getName());
+		}
+		
+		
+		//5. User 가 불변객체로 정의되었으므로 , 키보드 입력값으로 객체 생성한다면??
+		Scanner sc = new Scanner(System.in);
+		System.out.print("아이디 입력 >>> ");
+		String id = sc.nextLine();
+		System.out.print("이름 입력 >>> ");
+		String name = sc.nextLine();
+		System.out.print("나이 입력 >>> ");
+		int age = sc.nextInt();
+		
+		twice[5] = new User(id, name, age, 0);
+		System.out.println(twice[5].user());
+		
+		twice[6] = nayeon;			//이미 생성된 객체를 6번 인덱스로 참조합니다.
+		
+		
+		//6. twice 배열에 저장된 객체들의 age 필드값 평균을 구해보세요.
+		System.out.println("==twice 배열에 저장된 객체들의 age 필드값 평균 ==");
+		double sum=0;
+		for(int i=0;i<twice.length;i++) {
+			if(twice[i] !=null) 
+			  sum += twice[i].getAge();
+		}
+		System.out.println("합계=" + sum);
+		System.out.println("평균=" + sum/twice.length);   //배열요소 중 null 있다면 count 변수 필요합니다.
+		
+		System.out.println("==배열 요소 2개를 임의로 null 로 변경한 후 ==");
+		twice[4] = null;  twice[6]=null;
+		sum=0;
+		int count=0;
+		for(int i=0;i<twice.length;i++) {
+			if(twice[i] !=null)  {
+				count++;
+				sum += twice[i].getAge();
+			}
+		}
+		System.out.println("합계=" + sum);
+		System.out.println("null 이 아닌 객체의 개수=" + count);
+		System.out.println("평균=" + sum/count);
 	}
 
 }
+
+
+
+
+
+
+
+
+
+

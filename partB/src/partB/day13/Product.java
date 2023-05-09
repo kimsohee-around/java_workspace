722d85681d2a6ef2fe0a443d4f63dbb98d94c85b
kimsohee-around <koreait.sec2020@gmail.com>
2023-05-09 12:02:57 +0900

    #13 생성자 완료

--- partB/src/partB/day13/Product.java
+++ partB/src/partB/day13/Product.java
@@ -1,6 +1,6 @@
 package partB.day13;
 
-//쇼핑몰에서 구입할 상품정보를 저장하는 예시로 클래스 정의
+//쇼핑몰에서 구입할 상품정보를 저장하는 예시로 클래스 정의. 생성자 메소드 오버로딩이 핵심.
 public class Product {
 	//인스턴스 필드 (전역변수) 
 	private long code;
@@ -8,9 +8,10 @@ public class Product {
 	private long price;
 	private String company;
 	
-	//생성자는 메소드 입니다. 리턴타입은 없는 형식. 클래스 내부에 생성자 선언을 생략했다면, 컴파일러는 
+	//생성자는 `클래스와 이름이 같은 리턴타입은 없는 형식의 메소드` 입니다. 클래스 내부에 생성자 선언을 생략했다면, 컴파일러는 
 	//중괄호 { } 블록 내용이 비어있는 기본 생성자(Default Constructor)를 바이트 코드에 자동
-	//추가 시킨다.
+	//추가 시킨다. 생성자도 메소드이므로 오버로딩이 가능합니다.  
+	//this() 또는 this(매개변수들) 와 같이 생성자 호출도 가능합니다.
 	public Product() {
 		// 기본 생성자
 		// code 필드의 초기값 변경
@@ -23,6 +24,33 @@ public class Product {
 		this.productName = productName;
 	}
 	
+//	public Product(String company) {//메소드 시그니처(인자 형식과 개수) 동일한 2개가 동시에 존재 못함.
+//		this.company=company;
+//	}
+	
+	public Product(long price) {
+		this.price=price;
+	}
+	
+	public Product(long price,String company) {
+		this.price = price;
+		this.company = company;
+	}
+	
+	//실행 결과는?
+	public Product(String productName, long price,String company) {
+		this(productName);			//생성자 메소드 중에 String 매개변수 1개인 것 호출
+		this.price = price;
+		this.company = company;
+	}
+	
+	//모든 필드값을 초기화하는 생성자를 만들어 보세요.매개변수 4개를 모두 받는 형식.
+	public Product(long code,String productName, long price , String company) {
+		this.code=code;
+		this.productName=productName;
+		this.price=price;
+		this.company=company;
+	}
 	
 	//getter
 	public long getCode() { return this.code; }

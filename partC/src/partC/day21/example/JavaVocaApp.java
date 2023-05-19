790c15d2fc6ccda1e3388bb6e133dc3c3ed26b97
kimsohee-around <koreait.sec2020@gmail.com>
2023-05-19 13:53:20 +0900

    #21 map 단어장
    * 파일에서 읽은 한줄로 객체생성하여 put
    * 출력 print 구현

--- partC/src/partC/day21/example/JavaVocaApp.java
+++ partC/src/partC/day21/example/JavaVocaApp.java
@@ -7,13 +7,18 @@ public class JavaVocaApp {
 	//단어장 프로그램
 	public static void main(String[] args) {
 		
+		JavaVocaNote myNote = new JavaVocaNote();
+		
 		Scanner sc = new Scanner(System.in);
 		String english;
 		String korean;
 		String level;
 		String sel; 
 		boolean run = true;
+		String path = "D:\\iclass0419\\javavoca.txt";
 		//1.프로그램을 시작하면 저장된 단어들을 파일에서 읽어옵니다.
+		myNote.load(path);
+		
 		System.out.println("선택 기능 👉 [n]새 단어 저장 [s]단어 찾기 [g]그룹선택 보기   [a]전체보기 [e]프로그램 끝내기");
 		
 		while (run) { 
@@ -34,11 +39,14 @@ public class JavaVocaApp {
 				case "g":
 					break;
 				case "a":
+					//화면에 모두 출력 메소드 호출
+					myNote.print();
 					break;
 				case "e":
+					run=false;		//종료조건
 					break;
 			}	
-				
 		} //while end
+		System.out.println("::::::::: 너의 자바 단어장을 종료합니다. !!! :::::::::::::");
 	}//main end
 }
--- partC/src/partC/day21/example/JavaVocaNote.java
+++ partC/src/partC/day21/example/JavaVocaNote.java
@@ -5,10 +5,18 @@ import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 //단어장의 기능을 정의한 인터페이스 사용하기 - 자바 단어장 클래스로 구현.
 public class JavaVocaNote implements VocaNoteBook{
 
+	//파일에서 읽어온 데이터를 저장할 자료구조 필요합니다.
+	Map<String,JavaVoca> voca;		//String은 영어단어, JavaVoca 객체(영어단어,한글뜻,레벨)
+	
+	public JavaVocaNote() {
+		voca = new TreeMap<>();				//데이터 저장할 map 객체 생성
+	}
 	
 	@Override
 	public void load(String path) {		//파일에서 단어들 불러오기
@@ -17,7 +25,13 @@ public class JavaVocaNote implements VocaNoteBook{
 		try {
 			fsc = new Scanner(file);
 			while(fsc.hasNext()) {
-				System.out.println(fsc.nextLine());
+				String temp = fsc.nextLine();
+				System.out.println(temp);  
+				//public		공공의,공적인,대중			Beginner
+				// 읽어온 문자열로 JavaVoca 객체를 만들어서 map에 저장해야 합니다.
+				StringTokenizer stk = new StringTokenizer(temp,"\t_");
+				String name = stk.nextToken();
+				voca.put(name, new JavaVoca(name, stk.nextToken(), stk.nextToken()));
 			}
 			System.out.println("정상적으로 단어장 파일 읽어오기 끝!!");
 
@@ -31,7 +45,15 @@ public class JavaVocaNote implements VocaNoteBook{
 	
 	@Override
 	public void print() {
-		// TODO Auto-generated method stub
+		System.out.println(String.format("%-20s\t%-20s\t%-20s", "자바 단어","뜻","레벨"));
+		System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+		for(String key : voca.keySet()) {	
+				//iterator 대신에 향상 for사용 가능합니다.map 의 key값만으로 set 을 만들어야 합니다.
+				JavaVoca vo = voca.get(key);	//key값으로 value를 가져오기
+				System.out.println(String.format("%-20s\t%-20s\t%-20s", 
+						vo.getEnglish(),vo.getKorean(),vo.getLevel()));
+		}
+			
 		
 	}
 	

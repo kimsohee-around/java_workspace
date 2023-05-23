6dba9309d1b77b8fc23f6f002213b651a58d4934
kimsohee-around <koreait.sec2020@gmail.com>
2023-05-23 11:35:34 +0900

    #23 롬복테스트

---
+++ partC/src/partC/day23/Customer.java
@@ -0,0 +1,13 @@
+package partC.day23;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Customer {
+	private String name;
+	private int age;
+	private String address;
+
+}
